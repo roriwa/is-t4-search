df16a7b86aa6f104b235adac3f7ccdc939378e33
roriwa <103991412+roriwa@users.noreply.github.com>
2024-12-08 16:40:04 +0100

    implemented api query endpoint

--- src/t4search/api/__init__.py
+++ src/t4search/api/__init__.py
@@ -14,13 +14,74 @@ chroma_client = create_chroma_client()
 
 @api.get("/api/query", response_model=t.List[QueryResponseModel])
 def query(
-        topics: t.List[str] = fastapi.Query(min_length=1),
+        query_text: str = fastapi.Query(),
+        # topics: t.List[str] = fastapi.Query(default_factory=list),
         persons: t.List[str] =  fastapi.Query(default_factory=list),
         dates: t.List[str] = fastapi.Query(default_factory=list),
         parties: t.List[str] = fastapi.Query(default_factory=list),
+        limit: t.Optional[int] = fastapi.Query(default=10),
 ) -> t.List[QueryResponseModel]:
     dates: t.List[DateRange] = list(map(DateRange.from_string, dates))
-    return []
+
+    wheres = []
+    where = {'$and': wheres}
+
+    if persons:
+        wheres.append({
+            'person': {
+                '$in': persons,
+            }
+        })
+
+    if parties:
+        wheres.append({
+            'party': {
+                '$in': parties,
+            }
+        })
+
+    if dates:
+        date_ranges = []
+        for date in dates:
+            if date.start and date.end:
+                date_ranges.append({
+                    '$and': [
+                        { 'date': { '$gte': date.start } },
+                        { 'date': { '$lte': date.end } },
+                    ],
+                })
+            elif date.start:
+                date_ranges.append({
+                    'date': { '$gte': date.start },
+                })
+            elif date.end:
+                date_ranges.append({
+                    'date': { '$lte': date.end }
+                })
+            else:
+                raise
+        wheres.append({
+            '$or': date_ranges,
+        })
+
+
+    protocols = chroma_client.get_collection(name="protocols")
+    results = ChromaResponseObject.model_validate(protocols.query(
+        query_texts=[query_text],
+        where=where,
+        n_results=limit,
+    ))
+
+    n_results = len(results.documents[0])
+
+    return [
+        QueryResponseModel(
+            mongo_id=results.ids[0][i],
+            metadata=results.metadatas[0][i],
+            distances=results.distances[0][i],
+        )
+        for i in range(n_results)
+    ]
 
 
 def __main__(**kwargs):
--- src/t4search/api/models.py
+++ src/t4search/api/models.py
@@ -2,10 +2,11 @@
 r"""
 
 """
+import typing as t
 from pydantic import BaseModel
 
 
-__all__ = ['Metadata', 'QueryResponseModel']
+__all__ = ['Metadata', 'QueryResponseModel', 'ChromaResponseObject']
 
 
 class Metadata(BaseModel):
@@ -17,4 +18,14 @@ class Metadata(BaseModel):
 class QueryResponseModel(BaseModel):
     mongo_id: int
     metadata: Metadata
-    distance: float
+    distances: t.List[float]
+
+
+class ChromaResponseObject(BaseModel):
+    documents: t.List[t.List[str]]
+    ids: t.List[t.List[int]]
+    distances: t.List[t.List[float]]
+    uris: t.Optional[t.List[t.List[t.Any]]]
+    data: t.Optional[t.List[t.List[t.Any]]]
+    metadatas: t.List[t.List[t.Any]]
+    embeddings: t.Optional[t.List[t.List[t.Any]]]

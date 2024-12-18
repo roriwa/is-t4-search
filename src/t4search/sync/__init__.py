f37bba9f6e814cc67cf9a7400c9ad00540b87340
roriwa <103991412+roriwa@users.noreply.github.com>
2024-12-18 13:46:05 +0100

    added synced.json file to save already synced protocols and skip those

--- src/t4search/sync/__init__.py
+++ src/t4search/sync/__init__.py
@@ -2,8 +2,10 @@
 r"""
 
 """
+import json
 import logging
 import typing as t
+from pathlib import Path
 from functools import lru_cache
 import datetime
 import nltk
@@ -19,8 +21,9 @@ sync_lock = filelock.FileLock("sync.lock")
 def __main__():
     logging.info("initiating sync")
 
-    # helper
+    # tokenizer
 
+    logging.info("creating nltk tokenizer")
     sent_tokenizer = nltk.PunktTokenizer(lang="german")
 
     # creating connections
@@ -37,6 +40,19 @@ def __main__():
     chroma_client = create_chroma_client()
     chroma_protocol_collection = chroma_client.get_or_create_collection(name="protocols")
 
+    # loading synced
+
+    synced_protocols_file = Path.cwd() / "synced.json"
+
+    synced_protocols: t.List[int]
+    if synced_protocols_file.is_file():
+        logging.info("loading synced protocols from %s", synced_protocols_file)
+        with open(synced_protocols_file, "r") as f:
+            synced_protocols = json.load(fp=f)
+    else:
+        logging.info("creating synced protocols as file does not exist (%s)", synced_protocols_file)
+        synced_protocols = []
+
     # speaker fetching
 
     @lru_cache(maxsize=256)
@@ -49,25 +65,31 @@ def __main__():
     logging.info("Starting actual sync")
 
     for protocol in mongo_protocols_data:
-        logging.info("syncing protocol %s", protocol["id"])
+        protocol_id = protocol["id"]
+
+        if protocol_id in synced_protocols:
+            logging.info("Skipping protocol %s as it's already synced", protocol_id)
+            continue
+
+        logging.info("syncing protocol %s", protocol_id)
 
         date = datetime.datetime.strptime(protocol['datum'], '%d.%m.%Y').date().toordinal()
 
         for session_index, session in enumerate(protocol["sitzungsverlauf"]):
-            logging.info("syncing protocol %s session %d", protocol["id"], session_index)
+            logging.info("syncing protocol %s session %d", protocol_id, session_index)
 
             ids: t.List[str] = []
             documents: t.List[str] = []
             metadatas: t.List[t.Dict[str, t.Any]] = []
 
             for speach_index, speach in enumerate(session["rede"]):
-                logging.info("syncing %s - session #%d - speach #%d", protocol["id"], session_index, speach_index)
+                logging.info("syncing %d - session #%d - speach #%d", protocol_id, session_index, speach_index)
 
                 speaker_id = speach['redner_id']
                 speaker = get_speaker_by_id(speaker_id)
 
                 for sentence_index, sentence in enumerate(sent_tokenizer.tokenize(speach["text"])):
-                    ids.append(f"{protocol['id']}#{session_index}#{speach_index}#{sentence_index}")
+                    ids.append(f"{protocol_id}#{session_index}#{speach_index}#{sentence_index}")
                     logging.info("Sentence: %r", sentence)
                     documents.append(sentence)
                     metadatas.append(dict(
@@ -77,3 +99,9 @@ def __main__():
                     ))
 
             chroma_protocol_collection.upsert(ids=ids, documents=documents, metadatas=metadatas)
+
+        synced_protocols.append(protocol_id)
+
+        logging.info("writing synced protocols to %s", synced_protocols_file)
+        with open(synced_protocols_file, "w") as f:
+            json.dump(synced_protocols, fp=f)

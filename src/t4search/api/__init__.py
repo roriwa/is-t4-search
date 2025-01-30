64b283e8b48a2c2060f2557499092fdf5452e712
roriwa <103991412+roriwa@users.noreply.github.com>
2025-01-30 11:02:10 +0100

    added x-process-time response header

--- src/t4search/api/__init__.py
+++ src/t4search/api/__init__.py
@@ -2,6 +2,7 @@
 r"""
 
 """
+import time
 import typing as t
 import nltk
 import fastapi
@@ -11,6 +12,15 @@ from .models import *
 
 
 api = fastapi.FastAPI(title="T4Search API")
+
+
+@api.middleware("http")
+async def add_process_time_header(request: fastapi.Request, call_next: callable) -> fastapi.Response:
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 
 @api.post("/api/query", response_model=t.List[QueryResponseModel])
